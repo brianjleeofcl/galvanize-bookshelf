34a5abbbf57318fb68642d97b9b43ecbfed10404
Brian Lee <brianjleeofcl@gmail.com>
2017-01-05 15:39:22 -0800

    Part 1

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,23 @@
 'use strict';
 
 const express = require('express');
+const knex = require('../knex')
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+router.get('/books', (req, res, next) => {
+  knex('books').orderBy('id').then((array) => {
+    res.send(array);
+  }).catch(err => next(err));
+});
+
+router.get();
+
+router.post();
+
+router.patch();
+
+router.delete();
 
 module.exports = router;
